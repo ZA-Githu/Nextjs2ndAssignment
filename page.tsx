df4855537dbaac3a0594b190a76924406d79f773
ZA-Githu <zehraaunty123@gmail.com>
2024-09-29 22:26:16 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+
+import Header from "./components/Header";
+
+import About from "./components/About";
+
+export default function Home()  {
+  return (
+    <div>
+        <Header />
+        <About />
+    </div>
+  )
+}
